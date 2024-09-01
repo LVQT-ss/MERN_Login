d3d8157c9620aacb730f8d6618a8103c8eb7bb48
LVQT <levietquocthinh@gmail.com>
2024-09-02 01:14:36 +0700

    All routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,51 @@
-import './App.css';
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-function App() {
+// ALL COMPONENTS
+import Username from './components/Username';
+import Password from './components/Password';
+import Register from './components/Register';
+import Profile from './components/Profile';
+import Recovery from './components/Recovery';
+import Reset from './components/Reset';
+import PageNotFound from './components/PageNotFound';
+// root routes
+const router = createBrowserRouter([
+    {
+        path : '/',
+        element : <Username></Username>
+    },
+    {
+        path : '/register',
+        element : <Register></Register>
+    },
+    {
+        path : '/password',
+        element : <Password />
+    },
+    {
+        path : '/profile',
+        element : <Profile />
+    },
+    {
+        path : '/recovery',
+        element : <Recovery></Recovery>
+    },
+    {
+        path : '/reset',
+        element : <Reset></Reset>
+    },
+    {
+        path : '*',
+        element : <PageNotFound></PageNotFound>
+    },
+])
+export default function App() {
   return (
-    <div className="App">
-     <h1>React</h1>
-
-    </div>
-  );
+    <main>
+        <RouterProvider router={router}>
+           
+        </RouterProvider>
+    </main>
+  )
 }
-
-export default App;
